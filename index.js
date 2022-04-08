fb7c80fb03c8eb025d12fc529bcb5774a4029aa7
Mostafa Akbarzadeh <mostafa.akbarzadeh.asot@gmail.com>
2022-04-08 23:04:12 +0430

    Use new Board class

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// # not working in browser
+// import {Board} from './Board.js';
+// import {fs} from 'fs';
+
+const Board = require('./Board.js');
 const fs = require('fs');
 
 if(!process.argv[2])
@@ -31,70 +36,8 @@ fs.readFile(process.argv[2], 'utf8', (err, data) => {
     for(let num of rands.split(',').map((i) => parseInt(i)))
         for(let b of boards)
             if((score = b.find(num)) != undefined){
-                console.log(b.score, b.backup, num, score);
+                console.log(score);
                 return;
             }
-
+g
 })
-
-function Board(matrix){
-    this.matrix = matrix;
-    
-    this.backup = JSON.parse(JSON.stringify(this.matrix)).map((i) => i.fill(0));
-
-    this.score = 0;
-
-    this.find = function(number){
-
-        for(let row in this.matrix)
-            if((index = this.matrix[row].indexOf(number)) != -1){
-                this.backup[row][index] = 1
-
-                if(this.checkRow(row) || this.checkCol(index)){
-                    return (this.calcScore() * number);
-                }
-                break;
-            }
-        
-    }
-
-    this.calcScore = function(){
-
-        for(let row in this.backup)
-            for(let col = 0; col < this.backup[row].length; col++)
-                if(this.backup[row][col] == 0)
-                    this.score += this.matrix[row][col];
-
-        return this.score;   
-    }
-
-    this.checkRow = function(row){
-        if(this.backup[row].every((i) => i == 1))
-            return true;
-
-        return false;       
-    }
-
-    this.checkCol = function(col){
-        let found = true;
-
-        for(let row in this.backup)
-            if(this.backup[row][col] == 0){
-                found = false;
-                break;
-            }
-                
-        return found;
-    }
-}
-
-// const matrix = [
-//     ['1','5'],
-//     ['4','3']
-// ]
-
-// let b = new Board(matrix);
-// let total = 0;
-
-// console.log(b.find('1'));
-// console.log(b.find('4'));
